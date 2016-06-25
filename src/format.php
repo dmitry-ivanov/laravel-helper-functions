b7f3ae5c49394d4eeef3717481c46d4b25f2b384
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-06-25 14:13:02 +0300

    IHF: Format helpers added.

---
+++ src/format.php
@@ -0,0 +1,14 @@
+<?php
+
+if (!function_exists('format_bytes')) {
+    function format_bytes($bytes, $precision = 2)
+    {
+        $units = ['B', 'kB', 'MB', 'GB', 'TB'];
+
+        $maxFactor = count($units) - 1;
+        $factor = floor(((strlen($bytes) - 1) / 3));
+        $factor = ($factor > $maxFactor) ? $maxFactor : $factor;
+
+        return sprintf("%.{$precision}f", ($bytes / pow(1024, $factor))) . $units[$factor];
+    }
+}
