a04ecd4280f79151b8b55aecd82ed1ee3e6cc8c3
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-06-29 22:13:21 +0300

    IHF: Helpers renamed.

--- src/db.php
+++ src/db.php
@@ -2,23 +2,23 @@
 
 use Illuminate\Support\Facades\DB;
 
-if (!function_exists('laravel_db_is_mysql')) {
-    function laravel_db_is_mysql()
+if (!function_exists('db_is_mysql')) {
+    function db_is_mysql()
     {
         return (DB::getDefaultConnection() == 'mysql');
     }
 }
 
-if (!function_exists('laravel_db_mysql_now')) {
-    function laravel_db_mysql_now()
+if (!function_exists('db_mysql_now')) {
+    function db_mysql_now()
     {
         $result = (array) DB::selectOne('select now() as now');
         return $result['now'];
     }
 }
 
-if (!function_exists('laravel_db_mysql_variable')) {
-    function laravel_db_mysql_variable($name)
+if (!function_exists('db_mysql_variable')) {
+    function db_mysql_variable($name)
     {
         $result = (array) DB::selectOne('show variables where variable_name = ?', [$name]);
         return (isset($result['Value']) ? $result['Value'] : false);
