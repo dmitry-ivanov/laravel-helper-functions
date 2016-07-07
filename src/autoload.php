a7c9bc71c50c0d120630c0acf43d62a814139597
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-07-07 22:24:46 +0300

    IHF: Symfony finder used.

--- src/autoload.php
+++ src/autoload.php
@@ -1,6 +1,13 @@
 <?php
 
-$files = glob(__DIR__ . '/*.php');
+use Symfony\Component\Finder\Finder;
+
+$files = Finder::create()
+    ->files()
+    ->in(__DIR__)
+    ->name('*.php')
+    ->notName('autoload.php');
+
 foreach ($files as $file) {
     require_once $file;
 }
