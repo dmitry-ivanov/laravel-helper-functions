41d17723f9e22d52a4362a3586d69d9fac590558
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-09-13 20:58:49 +0300

    IHF: Initial dummy tests added.

---
+++ tests/ExampleTest.php
@@ -0,0 +1,10 @@
+<?php
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function it_calculates_2_on_2()
+    {
+        $this->assertEquals(4, 2*2);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,5 @@
+<?php
+
+abstract class TestCase extends PHPUnit_Framework_TestCase
+{
+}
