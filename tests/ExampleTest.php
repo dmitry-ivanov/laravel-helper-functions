d098409ca49e16b7da38124ca731f50ea6495707
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-09-13 21:09:16 +0300

    IHF: StyleCI thanks.

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -5,6 +5,6 @@ class ExampleTest extends TestCase
     /** @test */
     public function it_calculates_2_on_2()
     {
-        $this->assertEquals(4, 2*2);
+        $this->assertEquals(4, 2 * 2);
     }
 }
