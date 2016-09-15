8093f49cfc76a5b1465b73d857243415197a65e5
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-09-15 11:16:43 +0300

    IHF: Illuminate dependency removed.

--- src/str.php
+++ src/str.php
@@ -1,17 +1,15 @@
 <?php
 
-use Illuminate\Support\Str;
-
 if (!function_exists('str_lower')) {
     function str_lower($string)
     {
-        return Str::lower($string);
+        return mb_strtolower($string, 'UTF-8');
     }
 }
 
 if (!function_exists('str_upper')) {
     function str_upper($string)
     {
-        return Str::upper($string);
+        return mb_strtoupper($string, 'UTF-8');
     }
 }
