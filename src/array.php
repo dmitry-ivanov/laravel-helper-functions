8c6d6b211f6bcd1e8bdbac60b91a07b721d13c96
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-09-15 19:18:44 +0300

    IHF: Type hinting added. Multiple occurrences case handled.

--- src/array.php
+++ src/array.php
@@ -1,15 +1,14 @@
 <?php
 
 if (!function_exists('array_except_value')) {
-    function array_except_value($array, $value)
+    function array_except_value(array $array, $value)
     {
         if (!is_array($value)) {
             $value = [$value];
         }
 
         foreach ($value as $item) {
-            $key = array_search($item, $array);
-            if ($key !== false) {
+            while (($key = array_search($item, $array, true)) !== false) {
                 unset($array[$key]);
             }
         }
