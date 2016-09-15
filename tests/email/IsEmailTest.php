4cae06d18f99677c13a2c5fef26c2afb58cd9502
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-09-15 20:49:22 +0300

    IHF: `is_email` fully covered.

---
+++ tests/email/IsEmailTest.php
@@ -0,0 +1,69 @@
+<?php
+
+class IsEmailTest extends TestCase
+{
+    /** @test */
+    public function it_returns_false_for_null()
+    {
+        $this->assertFalse(is_email(null));
+    }
+
+    /** @test */
+    public function it_returns_false_for_boolean_true()
+    {
+        $this->assertFalse(is_email(true));
+    }
+
+    /** @test */
+    public function it_returns_false_for_boolean_false()
+    {
+        $this->assertFalse(is_email(false));
+    }
+
+    /** @test */
+    public function it_returns_false_for_integer()
+    {
+        $this->assertFalse(is_email(123));
+    }
+
+    /** @test */
+    public function it_returns_false_for_float()
+    {
+        $this->assertFalse(is_email(123.45));
+    }
+
+    /** @test */
+    public function it_returns_false_for_empty_array()
+    {
+        $this->assertFalse(is_email([]));
+    }
+
+    /** @test */
+    public function it_returns_false_for_array()
+    {
+        $this->assertFalse(is_email(['user@example.com']));
+    }
+
+    /** @test */
+    public function it_returns_false_for_an_object()
+    {
+        $this->assertFalse(is_email(new StdClass()));
+    }
+
+    /** @test */
+    public function it_returns_false_for_invalid_emails()
+    {
+        $this->assertFalse(is_email('user'));
+        $this->assertFalse(is_email('user@'));
+        $this->assertFalse(is_email('user@example'));
+        $this->assertFalse(is_email('user@example.'));
+    }
+
+    /** @test */
+    public function it_returns_true_for_valid_emails()
+    {
+        $this->assertTrue(is_email('user@example.com'));
+        $this->assertTrue(is_email('user.name@example.com'));
+        $this->assertTrue(is_email('user.name-long@example.com'));
+    }
+}
