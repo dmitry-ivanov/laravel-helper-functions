8b33a8c31e4f959ed397b808948bdaa47338b0c4
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-09-17 19:07:49 +0300

    IHF: `db_mysql_variable` helper fully covered by tests.

---
+++ tests/db/DbMysqlVariableTest.php
@@ -0,0 +1,35 @@
+<?php
+
+use Mockery as m;
+
+class DbMysqlVariableTest extends TestCase
+{
+    /** @test */
+    public function it_returns_false_for_unexisting_mysql_variable()
+    {
+        $mock = m::mock('alias:Illuminate\Support\Facades\DB');
+        $mock->shouldReceive('selectOne')
+            ->withArgs(['show variables where variable_name = ?', ['fake']])
+            ->once()
+            ->andReturnNull()
+        ;
+        $this->assertFalse(db_mysql_variable('fake'));
+    }
+
+    /** @test */
+    public function it_returns_value_for_known_mysql_variable()
+    {
+        $mock = m::mock('alias:Illuminate\Support\Facades\DB');
+        $mock->shouldReceive('selectOne')
+             ->withArgs(['show variables where variable_name = ?', ['hostname']])
+             ->once()
+             ->andReturnUsing(function () {
+                 $object = new StdClass();
+                 $object->Variable_name = 'hostname';
+                 $object->Value = 'localhost';
+                 return $object;
+             })
+        ;
+        $this->assertEquals('localhost', db_mysql_variable('hostname'));
+    }
+}
