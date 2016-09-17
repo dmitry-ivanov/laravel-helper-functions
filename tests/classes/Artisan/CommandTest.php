71b31b05ecce369073d23e94b89f900e30651fce
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-09-17 17:24:58 +0300

    IHF: CommandTest added.

--- tests/classes/Artisan/CommandTest.php
+++ tests/classes/Artisan/CommandTest.php
@@ -2,11 +2,27 @@
 
 namespace Illuminated\Helpers\Artisan;
 
+use Mockery as m;
 use PHPUnit_Framework_Error;
 use TestCase;
 
 class CommandTest extends TestCase
 {
+    public static $functions;
+
+    protected function setUp()
+    {
+        self::$functions = m::mock();
+
+        $phpBinaryMock = m::mock('overload:Symfony\Component\Process\PhpExecutableFinder');
+        $phpBinaryMock->shouldReceive('find')->with(false)->zeroOrMoreTimes()->andReturn('php');
+
+        $utilsMock = m::mock('alias:Symfony\Component\Process\ProcessUtils');
+        $utilsMock->shouldReceive('escapeArgument')->withAnyArgs()->zeroOrMoreTimes()->andReturnUsing(function ($value) {
+            return $value;
+        });
+    }
+
     /** @test */
     public function it_has_required_arguments()
     {
@@ -27,4 +43,18 @@ class CommandTest extends TestCase
         $command = Command::factory('test');
         $this->assertInstanceOf(Command::class, $command);
     }
+
+    /** @test */
+    public function it_can_run_command_in_background()
+    {
+        self::$functions->shouldReceive('exec')->with("(php artisan test command) > /dev/null 2>&1 &")->once();
+
+        $command = Command::factory('test command');
+        $command->runInBackground();
+    }
+}
+
+function exec($command)
+{
+    return CommandTest::$functions->exec($command);
 }
