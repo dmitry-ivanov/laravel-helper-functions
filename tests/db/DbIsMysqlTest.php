e7de87dad084689b6f1c836f07c24abc12901f79
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-10-27 21:03:07 +0300

    IHF: Simplified.

--- tests/db/DbIsMysqlTest.php
+++ tests/db/DbIsMysqlTest.php
@@ -1,13 +1,11 @@
 <?php
 
-use Mockery as m;
-
 class DbIsMysqlTest extends TestCase
 {
     /** @test */
     public function it_returns_true_if_laravel_database_default_connection_is_mysql()
     {
-        $mock = m::mock('alias:Illuminate\Support\Facades\DB');
+        $mock = Mockery::mock('alias:Illuminate\Support\Facades\DB');
         $mock->shouldReceive('getDefaultConnection')->withNoArgs()->once()->andReturn('mysql');
 
         $this->assertTrue(db_is_mysql());
@@ -16,7 +14,7 @@ class DbIsMysqlTest extends TestCase
     /** @test */
     public function it_returns_false_if_laravel_database_default_connection_is_not_mysql()
     {
-        $mock = m::mock('alias:Illuminate\Support\Facades\DB');
+        $mock = Mockery::mock('alias:Illuminate\Support\Facades\DB');
         $mock->shouldReceive('getDefaultConnection')->withNoArgs()->once()->andReturn('sqlite');
 
         $this->assertFalse(db_is_mysql());
