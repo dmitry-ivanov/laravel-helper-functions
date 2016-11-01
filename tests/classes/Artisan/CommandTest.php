ef845ac94d9d0df411b76b90b73be3233dc425b0
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2016-11-01 15:50:15 +0200

    IHF: Typo fixed.

--- tests/classes/Artisan/CommandTest.php
+++ tests/classes/Artisan/CommandTest.php
@@ -47,7 +47,7 @@ class CommandTest extends TestCase
     /** @test */
     public function it_can_run_command_in_background()
     {
-        $this->shouldRecieveExecCallOnceWith('(php artisan test:command) > /dev/null 2>&1 &');
+        $this->shouldReceiveExecCallOnceWith('(php artisan test:command) > /dev/null 2>&1 &');
 
         $command = Command::factory('test:command');
         $command->runInBackground();
@@ -56,7 +56,7 @@ class CommandTest extends TestCase
     /** @test */
     public function run_in_background_supports_before_subcommand()
     {
-        $this->shouldRecieveExecCallOnceWith('(before command && php artisan test:command) > /dev/null 2>&1 &');
+        $this->shouldReceiveExecCallOnceWith('(before command && php artisan test:command) > /dev/null 2>&1 &');
 
         $command = Command::factory('test:command', 'before command');
         $command->runInBackground();
@@ -65,7 +65,7 @@ class CommandTest extends TestCase
     /** @test */
     public function run_in_background_supports_after_subcommand()
     {
-        $this->shouldRecieveExecCallOnceWith('(php artisan test:command && after command) > /dev/null 2>&1 &');
+        $this->shouldReceiveExecCallOnceWith('(php artisan test:command && after command) > /dev/null 2>&1 &');
 
         $command = Command::factory('test:command', null, 'after command');
         $command->runInBackground();
@@ -74,7 +74,7 @@ class CommandTest extends TestCase
     /** @test */
     public function run_in_background_supports_before_and_after_subcommands_together()
     {
-        $this->shouldRecieveExecCallOnceWith('(before && php artisan test:command && after) > /dev/null 2>&1 &');
+        $this->shouldReceiveExecCallOnceWith('(before && php artisan test:command && after) > /dev/null 2>&1 &');
 
         $command = Command::factory('test:command', 'before', 'after');
         $command->runInBackground();
@@ -87,7 +87,7 @@ class CommandTest extends TestCase
      */
     public function it_adds_php_option_for_hhvm()
     {
-        $this->shouldRecieveExecCallOnceWith('(before && php --php artisan test:command && after) > /dev/null 2>&1 &');
+        $this->shouldReceiveExecCallOnceWith('(before && php --php artisan test:command && after) > /dev/null 2>&1 &');
 
         define('HHVM_VERSION', true);
         $command = Command::factory('test:command', 'before', 'after');
@@ -101,14 +101,14 @@ class CommandTest extends TestCase
      */
     public function it_supports_overriding_of_artisan_binary_through_constant()
     {
-        $this->shouldRecieveExecCallOnceWith('(before && php custom-artisan test:command && after) > /dev/null 2>&1 &');
+        $this->shouldReceiveExecCallOnceWith('(before && php custom-artisan test:command && after) > /dev/null 2>&1 &');
 
         define('ARTISAN_BINARY', 'custom-artisan');
         $command = Command::factory('test:command', 'before', 'after');
         $command->runInBackground();
     }
 
-    private function shouldRecieveExecCallOnceWith($with)
+    private function shouldReceiveExecCallOnceWith($with)
     {
         self::$functions->shouldReceive('exec')->with($with)->once();
     }
