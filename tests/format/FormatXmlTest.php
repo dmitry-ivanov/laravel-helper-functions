2f6f38fafb1c2754526e545f57c8e169b173c34a
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2017-02-16 23:45:14 +0200

    IHF: `format_xml` helper test fixed.

--- tests/format/FormatXmlTest.php
+++ tests/format/FormatXmlTest.php
@@ -6,9 +6,9 @@ class FormatXmlTest extends TestCase
     public function it_formats_passed_xml_string()
     {
         $unformatted = file_get_contents(__DIR__ . '/FormatXmlTest/unformatted.xml');
-        $expected = file_get_contents(__DIR__ . '/FormatXmlTest/formatted.xml');
+        $formatted = file_get_contents(__DIR__ . '/FormatXmlTest/formatted.xml');
 
-        $this->assertEquals($expected, format_xml($unformatted));
+        $this->assertEquals($formatted, format_xml($unformatted));
     }
 
     /** @test */
