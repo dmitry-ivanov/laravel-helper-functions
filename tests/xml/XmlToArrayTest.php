160f1655f1f4fa601b69edc9ef9d903eb0675792
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2017-02-16 10:35:39 +0200

    IHF: `xml_to_array` tests added.

---
+++ tests/xml/XmlToArrayTest.php
@@ -0,0 +1,34 @@
+<?php
+
+class XmlToArrayTest extends TestCase
+{
+    /** @test */
+    public function it_transforms_xml_string_to_array()
+    {
+        $xml = file_get_contents(__DIR__ . '/XmlToArrayTest/example.xml');
+
+        $expected = [
+            'tasks' => [
+                'task' => [
+                    0 => [
+                        'to' => 'John',
+                        'from' => 'Jane',
+                        'title' => 'Go to the shop',
+                    ],
+                    1 => [
+                        'to' => 'John',
+                        'from' => 'Paul',
+                        'title' => 'Finish the report',
+                    ],
+                    2 => [
+                        'to' => 'Jane',
+                        'from' => 'Jeff',
+                        'title' => 'Clean the house',
+                    ],
+                ],
+            ],
+        ];
+
+        $this->assertEquals($expected, xml_to_array($xml));
+    }
+}
