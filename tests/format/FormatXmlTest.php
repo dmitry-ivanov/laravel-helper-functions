6c501bee8ceb7d5404201961b5f2bd52cd4c0cf2
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2017-02-16 23:43:56 +0200

    IHF: `format_xml` helper tests added.

--- tests/format/FormatXmlTest.php
+++ tests/format/FormatXmlTest.php
@@ -10,4 +10,12 @@ class FormatXmlTest extends TestCase
 
         $this->assertEquals($expected, format_xml($unformatted));
     }
+
+    /** @test */
+    public function it_throws_an_exception_if_non_xml_string_passed()
+    {
+        $this->willSeeException(Exception::class, 'String could not be parsed as XML');
+
+        format_xml('Non XML');
+    }
 }
