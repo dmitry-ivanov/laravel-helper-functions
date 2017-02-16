d185cfcd79f79d13b47b38d2a4b5f1926074e557
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2017-02-16 23:54:41 +0200

    IHF: `array_to_xml` helper tests added.

--- tests/xml/ArrayToXmlTest.php
+++ tests/xml/ArrayToXmlTest.php
@@ -66,4 +66,32 @@ class ArrayToXmlTest extends TestCase
 
         $this->assertEquals($expected, array_to_xml($array));
     }
+
+    /** @test */
+    public function is_supports_custom_root_element_name()
+    {
+        $array = [
+            'task' => [
+                0 => [
+                    'to' => 'John',
+                    'from' => 'Jane',
+                    'title' => 'Go to the shop',
+                ],
+                1 => [
+                    'to' => 'John',
+                    'from' => 'Paul',
+                    'title' => 'Finish the report',
+                ],
+                2 => [
+                    'to' => 'Jane',
+                    'from' => 'Jeff',
+                    'title' => 'Clean the house',
+                ],
+            ],
+        ];
+
+        $expected = file_get_contents(__DIR__ . '/ArrayToXmlTest/with-custom-root.xml');
+
+        $this->assertEquals($expected, array_to_xml($array, 'tasks'));
+    }
 }
