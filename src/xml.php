e5e98c0506583b5911dd2710d0b4752ec9a0e6a7
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2017-02-16 13:43:50 +0200

    IHF: `array_to_xml` helper added.

--- src/xml.php
+++ src/xml.php
@@ -1,10 +1,19 @@
 <?php
 
+use Spatie\ArrayToXml\ArrayToXml;
+
 if (!function_exists('xml_to_array')) {
     function xml_to_array($xml)
     {
         $xml = ($xml instanceof SimpleXMLElement) ? $xml : new SimpleXMLElement($xml);
 
         return json_decode(json_encode($xml), true);
+    }
+}
+
+if (!function_exists('array_to_xml')) {
+    function array_to_xml(array $array, $rootElementName = '', $replaceSpacesByUnderScoresInKeyNames = true)
+    {
+        return ArrayToXml::convert($array, $rootElementName, $replaceSpacesByUnderScoresInKeyNames);
     }
 }
