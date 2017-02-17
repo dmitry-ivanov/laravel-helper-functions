c287ba0741ce4623755ae0bef65b4792fa8c3f5c
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2017-02-17 15:29:59 +0200

    IHF: Moved backtrace methods to separate debug file.

--- src/format.php
+++ src/format.php
@@ -49,34 +49,3 @@ if (!function_exists('format_xml')) {
         return $dom->saveXML();
     }
 }
-
-if (!function_exists('backtrace_as_string')) {
-    function backtrace_as_string()
-    {
-        ob_start();
-
-        debug_print_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
-        $backtrace = ob_get_contents();
-
-        ob_end_clean();
-
-        return $backtrace;
-    }
-}
-
-if (!function_exists('minimized_backtrace_as_string')) {
-    function minimized_backtrace_as_string()
-    {
-        $minimized = [];
-
-        $backtrace = backtrace_as_string();
-        $backtrace = explode("\n", $backtrace);
-        foreach ($backtrace as $item) {
-            if (preg_match('/(#\d+) .*? called at \[(.*?)\]/', $item, $matches)) {
-                $minimized[] = $matches[1] . ' ' . $matches[2];
-            }
-        }
-
-        return implode("\n", $minimized);
-    }
-}
