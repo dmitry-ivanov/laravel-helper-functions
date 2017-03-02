63fc5034b81a9c8d552c33c23875f1ac1b700593
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2017-03-02 14:31:03 +0200

    IHF: `multiarray_sort_by` tests added.

--- tests/array/MultiarraySortByTest.php
+++ tests/array/MultiarraySortByTest.php
@@ -57,7 +57,7 @@ class MultiarraySortByTest extends TestCase
     }
 
     /** @test */
-    public function it_can_sort_by_two_fields_without_specifying_sort_order()
+    public function it_can_sort_by_two_fields_without_specifying_sort_order_and_asc_would_be_used_for_both()
     {
         $array = [
             ['name' => 'Mercedes-Benz', 'model' => 'GLS', 'price' => 120000],
@@ -74,5 +74,65 @@ class MultiarraySortByTest extends TestCase
         ];
 
         $this->assertEquals($expected, multiarray_sort_by($array, 'name', 'model'));
+    }
+
+    /** @test */
+    public function it_can_sort_by_two_fields_with_specifying_asc_sort_order_only_for_the_first()
+    {
+        $array = [
+            ['name' => 'Mercedes-Benz', 'model' => 'GLS', 'price' => 120000],
+            ['name' => 'Mercedes-Benz', 'model' => 'GLE Coupe', 'price' => 110000],
+            ['name' => 'BMW', 'model' => 'X6', 'price' => 77000],
+            ['name' => 'Porsche', 'model' => 'Cayenne', 'price' => 117000],
+        ];
+
+        $expected = [
+            ['name' => 'BMW', 'model' => 'X6', 'price' => 77000],
+            ['name' => 'Mercedes-Benz', 'model' => 'GLE Coupe', 'price' => 110000],
+            ['name' => 'Mercedes-Benz', 'model' => 'GLS', 'price' => 120000],
+            ['name' => 'Porsche', 'model' => 'Cayenne', 'price' => 117000],
+        ];
+
+        $this->assertEquals($expected, multiarray_sort_by($array, 'name', SORT_ASC, 'model'));
+    }
+
+    /** @test */
+    public function it_can_sort_by_two_fields_with_specifying_asc_sort_order_only_for_the_second()
+    {
+        $array = [
+            ['name' => 'Mercedes-Benz', 'model' => 'GLS', 'price' => 120000],
+            ['name' => 'Mercedes-Benz', 'model' => 'GLE Coupe', 'price' => 110000],
+            ['name' => 'BMW', 'model' => 'X6', 'price' => 77000],
+            ['name' => 'Porsche', 'model' => 'Cayenne', 'price' => 117000],
+        ];
+
+        $expected = [
+            ['name' => 'BMW', 'model' => 'X6', 'price' => 77000],
+            ['name' => 'Mercedes-Benz', 'model' => 'GLE Coupe', 'price' => 110000],
+            ['name' => 'Mercedes-Benz', 'model' => 'GLS', 'price' => 120000],
+            ['name' => 'Porsche', 'model' => 'Cayenne', 'price' => 117000],
+        ];
+
+        $this->assertEquals($expected, multiarray_sort_by($array, 'name', 'model', SORT_ASC));
+    }
+
+    /** @test */
+    public function it_can_sort_by_two_fields_with_specifying_asc_sort_order_for_both()
+    {
+        $array = [
+            ['name' => 'Mercedes-Benz', 'model' => 'GLS', 'price' => 120000],
+            ['name' => 'Mercedes-Benz', 'model' => 'GLE Coupe', 'price' => 110000],
+            ['name' => 'BMW', 'model' => 'X6', 'price' => 77000],
+            ['name' => 'Porsche', 'model' => 'Cayenne', 'price' => 117000],
+        ];
+
+        $expected = [
+            ['name' => 'BMW', 'model' => 'X6', 'price' => 77000],
+            ['name' => 'Mercedes-Benz', 'model' => 'GLE Coupe', 'price' => 110000],
+            ['name' => 'Mercedes-Benz', 'model' => 'GLS', 'price' => 120000],
+            ['name' => 'Porsche', 'model' => 'Cayenne', 'price' => 117000],
+        ];
+
+        $this->assertEquals($expected, multiarray_sort_by($array, 'name', SORT_ASC, 'model', SORT_ASC));
     }
 }
