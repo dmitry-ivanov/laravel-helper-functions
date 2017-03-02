029a0f955cb3a99cc786a5945ebcee4a26ee68be
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2017-03-02 14:23:27 +0200

    IHF: `multiarray_sort_by` tests added.

---
+++ tests/array/MultiarraySortByTest.php
@@ -0,0 +1,40 @@
+<?php
+
+class MultiarraySortByTest extends TestCase
+{
+    /** @test */
+    public function it_can_sort_by_one_field_without_specifying_sort_order_which_is_asc_by_default()
+    {
+        $array = [
+            ['name' => 'Mercedes-Benz', 'model' => 'GLE Coupe', 'price' => 110000],
+            ['name' => 'BMW', 'model' => 'X6', 'price' => 77000],
+            ['name' => 'Porsche', 'model' => 'Cayenne', 'price' => 117000],
+        ];
+
+        $expected = [
+            ['name' => 'BMW', 'model' => 'X6', 'price' => 77000],
+            ['name' => 'Mercedes-Benz', 'model' => 'GLE Coupe', 'price' => 110000],
+            ['name' => 'Porsche', 'model' => 'Cayenne', 'price' => 117000],
+        ];
+
+        $this->assertEquals($expected, multiarray_sort_by($array, 'price'));
+    }
+
+    /** @test */
+    public function it_can_sort_by_one_field_with_specifying_sort_order()
+    {
+        $array = [
+            ['name' => 'Mercedes-Benz', 'model' => 'GLE Coupe', 'price' => 110000],
+            ['name' => 'BMW', 'model' => 'X6', 'price' => 77000],
+            ['name' => 'Porsche', 'model' => 'Cayenne', 'price' => 117000],
+        ];
+
+        $expected = [
+            ['name' => 'Porsche', 'model' => 'Cayenne', 'price' => 117000],
+            ['name' => 'Mercedes-Benz', 'model' => 'GLE Coupe', 'price' => 110000],
+            ['name' => 'BMW', 'model' => 'X6', 'price' => 77000],
+        ];
+
+        $this->assertEquals($expected, multiarray_sort_by($array, 'price', SORT_DESC));
+    }
+}
