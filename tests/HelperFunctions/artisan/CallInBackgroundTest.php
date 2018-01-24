17d8e92947acedfb987d66893595b8eb2ea4883d
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2018-01-24 13:48:49 +0200

    Mockery 1.0 features used.

--- tests/HelperFunctions/artisan/CallInBackgroundTest.php
+++ tests/HelperFunctions/artisan/CallInBackgroundTest.php
@@ -3,7 +3,6 @@
 namespace Illuminated\Helpers\HelperFunctions\Tests\Artisan;
 
 use Illuminated\Helpers\HelperFunctions\Tests\TestCase;
-use Mockery;
 
 /**
  * @runTestsInSeparateProcesses
@@ -14,7 +13,7 @@ class CallInBackgroundTest extends TestCase
     /** @test */
     public function it_works_without_optional_before_and_after_parameters()
     {
-        $mock = Mockery::mock('alias:Illuminated\Helpers\Artisan\Command');
+        $mock = mock('alias:Illuminated\Helpers\Artisan\Command');
         $mock->shouldReceive('factory')->withArgs(['test command', null, null])->once()->andReturnSelf();
         $mock->shouldReceive('runInBackground')->withNoArgs()->once();
 
@@ -24,7 +23,7 @@ class CallInBackgroundTest extends TestCase
     /** @test */
     public function it_works_with_optional_before_parameter()
     {
-        $mock = Mockery::mock('alias:Illuminated\Helpers\Artisan\Command');
+        $mock = mock('alias:Illuminated\Helpers\Artisan\Command');
         $mock->shouldReceive('factory')->withArgs(['test command', 'before command', null])->once()->andReturnSelf();
         $mock->shouldReceive('runInBackground')->withNoArgs()->once();
 
@@ -34,7 +33,7 @@ class CallInBackgroundTest extends TestCase
     /** @test */
     public function it_works_with_optional_after_parameter()
     {
-        $mock = Mockery::mock('alias:Illuminated\Helpers\Artisan\Command');
+        $mock = mock('alias:Illuminated\Helpers\Artisan\Command');
         $mock->shouldReceive('factory')->withArgs(['test command', null, 'after command'])->once()->andReturnSelf();
         $mock->shouldReceive('runInBackground')->withNoArgs()->once();
 
@@ -44,7 +43,7 @@ class CallInBackgroundTest extends TestCase
     /** @test */
     public function it_works_with_optional_before_and_after_parameters_together()
     {
-        $mock = Mockery::mock('alias:Illuminated\Helpers\Artisan\Command');
+        $mock = mock('alias:Illuminated\Helpers\Artisan\Command');
         $mock->shouldReceive('factory')->withArgs(['test command', 'before', 'after'])->once()->andReturnSelf();
         $mock->shouldReceive('runInBackground')->withNoArgs()->once();
 
