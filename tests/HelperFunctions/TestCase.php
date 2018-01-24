6dab25fc86f3a3ee23cd80f362edf1b404d28046
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2018-01-24 13:22:40 +0200

    New mockery phpunit integration trait used.

--- tests/HelperFunctions/TestCase.php
+++ tests/HelperFunctions/TestCase.php
@@ -4,10 +4,12 @@ namespace Illuminated\Helpers\HelperFunctions\Tests;
 
 use Illuminated\Testing\TestingTools;
 use Mockery;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 
 abstract class TestCase extends \PHPUnit\Framework\TestCase
 {
     use TestingTools;
+    use MockeryPHPUnitIntegration;
 
     public static $functions;
 
@@ -19,10 +21,5 @@ abstract class TestCase extends \PHPUnit\Framework\TestCase
     protected function shouldReceiveExecCallOnceWith($with)
     {
         self::$functions->shouldReceive('exec')->with($with)->once();
-    }
-
-    protected function tearDown()
-    {
-        Mockery::close();
     }
 }
