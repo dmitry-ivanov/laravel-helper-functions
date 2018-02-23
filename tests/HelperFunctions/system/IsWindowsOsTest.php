5e0d56da9686d934d54cb3533ce090b571b080a2
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2018-02-23 11:13:20 +0200

    `is_windows_os` helper function tests added.

---
+++ tests/HelperFunctions/system/IsWindowsOsTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Illuminated\Helpers\System;
+
+use Illuminated\Helpers\HelperFunctions\Tests\TestCase;
+
+class IsWindowsOsTest extends TestCase
+{
+    /** @test */
+    public function it_returns_false_on_non_windows_os()
+    {
+        $this->emulateLinuxOs();
+
+        $this->assertFalse(is_windows_os());
+    }
+
+    /** @test */
+    public function it_returns_true_on_windows_os()
+    {
+        $this->emulateWindowsOs();
+
+        $this->assertTrue(is_windows_os());
+    }
+}
+
+if (!function_exists(__NAMESPACE__ . '\php_uname')) {
+    function php_uname()
+    {
+        return TestCase::$functions->php_uname();
+    }
+}
