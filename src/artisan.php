8c3017c1b1534d3702282a6d9b373b814f0c8337
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2019-09-12 19:34:04 +0300

    Remove void return

--- src/artisan.php
+++ src/artisan.php
@@ -5,6 +5,6 @@ use Illuminated\Helpers\Artisan\Command;
 if (!function_exists('call_in_background')) {
     function call_in_background($command, $before = null, $after = null)
     {
-        return Command::factory($command, $before, $after)->runInBackground();
+        Command::factory($command, $before, $after)->runInBackground();
     }
 }
