9ba4376c82202746767d8cb8d60e45239b8d9d0e
Dmitry Ivanov <dmitry.g.ivanov@gmail.com>
2022-03-10 18:56:33 +0200

    Add types

--- src/email.php
+++ src/email.php
@@ -5,11 +5,8 @@ use Illuminate\Support\Arr;
 if (!function_exists('is_email')) {
     /**
      * Check whether the given string is an email address or not.
-     *
-     * @param mixed $string
-     * @return bool
      */
-    function is_email($string)
+    function is_email(mixed $string): bool
     {
         return (bool) filter_var($string, FILTER_VALIDATE_EMAIL);
     }
@@ -21,11 +18,8 @@ if (!function_exists('to_rfc2822_email')) {
      *
      * @see http://faqs.org/rfcs/rfc2822.html
      * @see https://php.net/manual/en/function.mail.php
-     *
-     * @param array $addresses
-     * @return string
      */
-    function to_rfc2822_email(array $addresses)
+    function to_rfc2822_email(array $addresses): string
     {
         // Check if we're dealing with one address, without multiarray
         $addresses = !empty($addresses['address']) ? [$addresses] : $addresses;
@@ -51,11 +45,8 @@ if (!function_exists('to_swiftmailer_emails')) {
      * Convert addresses data to SwiftMailer-suitable format.
      *
      * @see https://swiftmailer.org/docs/messages.html
-     *
-     * @param array $addresses
-     * @return array
      */
-    function to_swiftmailer_emails(array $addresses)
+    function to_swiftmailer_emails(array $addresses): array
     {
         // Check if we're dealing with one address, without multiarray
         $addresses = !empty($addresses['address']) ? [$addresses] : $addresses;
